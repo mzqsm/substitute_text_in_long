b64fd3a69d6910141e0ed5bf70d0d3137fd4f4eb
mzqsm <a104748001@163.com>
2024-11-26 15:40:46 +0800

    222

--- substitute_content_in_text.c
+++ substitute_content_in_text.c
@@ -1,137 +1,167 @@
 #include <stdio.h>
-#include <errno.h>
-#include <stdint.h>
-#include <string.h>
-#include <inttypes.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <errno.h>
+#include <stddef.h>
 
-#define MAX_SIZE 200
+#define MAX_SIZE 256
 
-errno_t replace_words_in_log(const char* filename, char* initial_content, char* replaced_content);
+errno_t replace_specially_words(const char* filename, char old_content[MAX_SIZE], char replaced_content[MAX_SIZE]);
 
 int main(void)
 {
-	int32_t if_mistake = 0;
+	errno = 0;
 
-	const char* filename = "C:\\Users\\H\\Desktop\\编程222用.log";//等修改完把路径再改回来
-
-	char initial_content[MAX_SIZE] = { 0 };
-
+	char old_content[MAX_SIZE] = { 0 };
 	char replaced_content[MAX_SIZE] = { 0 };
 
-	puts("plz input the sentence u want to be substituted!");
+	puts("plz input the sentence u want to be replaced!");
 
-	scanf_s("%99[^\n]", initial_content, MAX_SIZE);
+	int32_t check_first_scanf = scanf_s("%99[^\n]", old_content, (unsigned int)sizeof(old_content));
 
-	puts("plz enter the sentence u want to replace");
-
-	(void)getchar();
-
-	scanf_s("%99[^\n]", replaced_content, MAX_SIZE);
-
-	(void)getchar();
-
-	errno_t res = replace_words_in_log(filename, initial_content, replaced_content);
-
-	if (res)
+	if (check_first_scanf < 0)
 	{
-		char error_msg[200];
-		strerror_s(error_msg, MAX_SIZE, res);
-		fprintf(stderr, "An error occured : %s\n", error_msg);
+		perror("the first scanf_s error");
+		exit(EXIT_FAILURE);
+	}
+
+	(void)getchar();
+
+	puts("plz enter the sentence u want to substitute!");
+
+	int32_t check_second_scanf = scanf_s("%99[^\n]", replaced_content, (unsigned int)sizeof(replaced_content));
+
+	if (check_second_scanf < 0)
+	{
+		perror("the second scanf_s error");
+		exit(EXIT_FAILURE);
+	}
+
+	const char* filename = "C:\\Users\\H\\Desktop\\编程222用.txt";
+
+	(void)getchar();
+
+	errno_t err = replace_specially_words(filename, old_content, replaced_content);
+
+	if (errno)
+	{
+		perror("mistake occured");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		puts("input finished!");
+		puts("No error happened!");
 	}
 
-	_fcloseall();
+	int32_t if_fcloseall = _fcloseall();
 
-	perror("check if mistake :");
-
-	return 0;
+	exit(if_fcloseall == EOF ? EXIT_FAILURE : EXIT_SUCCESS);
 }
 
-errno_t replace_words_in_log(const char* filename, char* initial_content, char* replaced_content)
+
+errno_t replace_specially_words(const char* filename, char old_content[MAX_SIZE], char replaced_content[MAX_SIZE])
 {
 	FILE* stream = NULL;
 
-	if (filename == NULL || initial_content == NULL || replaced_content == NULL)
+	errno_t if_open = fopen_s(&stream, filename, "r+");
+
+	if (if_open)
 	{
-		return EINVAL;
-	}
-
-	errno_t err = fopen_s(&stream, filename, "r+");
-
-	if (err != 0 || stream == NULL)
-	{
-		char error_msg[100];
-
-		strerror_s(error_msg, sizeof(error_msg), errno);
-
-		fprintf(stderr, "Failed to open the file, because : %s\n", error_msg);
-
+		perror("there is some mistakes while opening the target file");
 		exit(EXIT_FAILURE);
 	}
 
 	char buffer[MAX_SIZE] = { 0 };
 
+	int32_t is_found = 1;
+
 	long pos = 0;
 
-	int32_t is_found = 0;
+	long new_pos = 0;
 
 	while (fgets(buffer, MAX_SIZE, stream) != NULL)
 	{
-		printf("texeeeeeee: %d\n",ftell(stream));
 		buffer[strcspn(buffer, "\n")] = '\0';
 
-		if (strcmp(buffer, initial_content) == 0)
+		is_found = strcmp(buffer, old_content);
+
+		printf("%d\n", is_found);
+
+		if (!is_found)
 		{
-			pos = ftell(stream) - (long)strlen(buffer) - 2;//hello\n\0.
-
-			printf("text1: %d\n", ftell(stream));
-
-			printf("text2:%d\n", strlen(buffer));
-			is_found = 1;
-
+			new_pos = ftell(stream) - strlen(buffer) - 2;
+			printf("%d", new_pos);
 			break;
 		}
 	}
 
-	if (is_found)
+	if (!is_found)
 	{
-		fseek(stream, pos, SEEK_SET);
+		rewind(stream);
 
-		size_t replace_len = strlen(replaced_content);
-		size_t search_len = strlen(initial_content);
+		char temporary_index[MAX_SIZE] = { 0 };
 
-		if (replace_len > MAX_SIZE - 1 || search_len > MAX_SIZE - 1)
+		fread(temporary_index, sizeof(char), MAX_SIZE, stream);
+
+		char* target_in_console = strstr(temporary_index, old_content);
+
+		if (target_in_console != NULL)
 		{
-			fclose(stream);
+			ptrdiff_t pos_before_target = target_in_console - temporary_index;
 
-			return ERANGE;
-		}
-		printf("final test: %d", ftell(stream));
+			char before_content[MAX_SIZE] = { 0 };
 
-		char input_index[MAX_SIZE] = { 0 };
+			strncpy_s(before_content, MAX_SIZE, temporary_index, (rsize_t)pos_before_target);
 
-		strcpy_s(input_index, MAX_SIZE, replaced_content);
+			rewind(stream);
 
-		strcat_s(input_index, MAX_SIZE, "\r\n");
+			size_t actual_len = strlen(before_content);
 
-		int32_t index = fputs(input_index, stream);
+			fwrite(before_content, sizeof(char), actual_len, stream);
 
-		if (index == EOF)
-		{
-			fclose(stream);
-			return errno;
+			pos = ftell(stream) + (long)strlen(buffer) + 2;
+
+			char sub_content[MAX_SIZE] = { 0 };
+
+			strcpy_s(sub_content, strlen(replaced_content) + 1, replaced_content);
+
+			if (strlen(replaced_content) < strlen(old_content))
+			{
+				size_t diff = strlen(old_content) - strlen(replaced_content);
+
+				fwrite(sub_content, sizeof(char), strlen(sub_content), stream);
+
+				for (size_t index = 0; index < diff; index++)
+				{
+					fputc(" ", stream);
+				}
+			}
+			else if (strlen(replaced_content) > strlen(old_content))
+			{
+				fseek(stream, 0, SEEK_END);
+
+				long end_pos = ftell(stream);
+
+				fseek(stream, pos, SEEK_SET);
+
+				char end_content[MAX_SIZE] = { 0 };
+
+				fread(end_content, sizeof(char), (size_t)(end_pos - pos), stream);
+
+				fseek(stream, new_pos, SEEK_SET);
+
+				fwrite(sub_content, sizeof(char), strlen(sub_content), stream);
+
+				fputs("\r\n", stream);
+
+				fputs(end_content, stream);
+			}
+			else
+			{
+				fwrite(sub_content, sizeof(char), strlen(sub_content), stream);
+			}
 		}
 	}
-	else
-	{
-		fclose(stream);
-		return ENOENT;
-	}
-
-	fclose(stream);
-	return 0;
 }
